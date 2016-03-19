3b76f1b2d635410ac9c32a805167ff968707ab62
MacKentoch <erwan.datin@gmail.com>
2016-03-19 14:30:53 +0100

    chore : add babel polyfill (help poor IE to understand js...)

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import 'babel-polyfill';
 import React                from 'react';
 import ReactDOM             from 'react-dom';
 import injectTpEventPlugin  from 'react-tap-event-plugin';
